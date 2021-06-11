84c3ad444882da55ef18152f954248b49630c367
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-06-11 15:51:45 -0300

    Pilha Rubro-Negra WIP

---
+++ StackDataStruct/EPilhaVazia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStruct
+{
+    public class EPilhaVazia : Exception
+    {
+        public EPilhaVazia(string message) : base(message)
+        {
+        }
+    }
+}
