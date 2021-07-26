9ce47e8f3c0e3bf18fa370ac6b29c17862dbfe5d
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-07-26 19:56:26 -0300

    Binary search tree init

---
+++ ArvoreBinariaDePesquisa/ArvoreBinariaDePesquisa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArvoreBinariaDePesquisa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
