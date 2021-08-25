56795a3ab032a49952c2002be3e7af9ed0ba0037
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-08-25 13:43:23 -0300

    Fila de Prioridade INIT

---
+++ TADFilaPrioridadeHeapArray/TADFilaPrioridadeHeapArray/Elemento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TADFilaPrioridadeHeapArray
+{
+    class Elemento
+    {
+        public int Key { get; set; }
+        public object Content { get; set; }
+    }
+}
---
+++ TADFilaPrioridadeHeapArray/TADFilaPrioridadeHeapArray/FilaPrioridadeHeapArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TADFilaPrioridadeHeapArray
+{
+    class FilaPrioridadeHeapArray
+    {
+        private Elemento[] arr;
+        private int tamanho;
+    }
+}
